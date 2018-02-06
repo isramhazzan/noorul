90f2355069548b680b59a5e4161f26b0e329f0ac
isramhazzan <35652605+isramhazzan@users.noreply.github.com>
2018-02-06 16:23:31 +0530

    Create 1positive or negative.c

---
+++ 1positive or negative.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include#comio.h>
+void main()
+{
+ int n;
+ printf("\n enter the value :");
+ scanf("%d",&n);
+ if(n<0.0)
+ printf("\n it is negative");
+ else if(n0.0)
+  printf("\n it is positive");
+  }
+  else
+  {
+  printf("\n entered 0");
+  }
+  getch();
+ } 
