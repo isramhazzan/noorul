e8fafc24fb51847d0f4d1943887d3ba00237f062
isramhazzan <35652605+isramhazzan@users.noreply.github.com>
2018-02-09 16:40:19 +0530

    Create 9NK2number.c

---
+++ 9NK2number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int N,K,a[10],sum=0;
+ printf(" enter the two number");
+ scanf("%d%d",&N,&K);
+ if(i=0;i<=N;i++)
+ { 
+ scanf("%d",&a[i]);
+ }
+ if(N>K)
+ for(i=0;i<K;i++)
+ {
+  sum=sum+i;
+  printf("%d",sum);
+ }
+ else 
+ {
+  printf("not valide);
+ }
+ getch();
+} 
+ 
