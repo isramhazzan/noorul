c8466fc5773483a37ef5af89f5dc2b72e89759a2
isramhazzan <35652605+isramhazzan@users.noreply.github.com>
2018-02-09 16:10:32 +0530

    Create 6leapyear.c

---
+++ 6leapyear.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int A;
+ scanf("%d",&A);
+ if(A%4==0)
+ {
+  printf("\n leap year");
+ }
+ else
+ {
+  printf("\n not leap year");
+ }
+ getch();
+} 
