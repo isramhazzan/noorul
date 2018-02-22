405ff0bb9563f5e120eeff86a8c5f53f30bc9793
isramhazzan <35652605+isramhazzan@users.noreply.github.com>
2018-02-22 15:46:26 +0530

    Create 18 arm no.c

---
+++ 18 arm no.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int n1,n2,temp,sum=0,x,a,i;
+clrscr();
+scanf("%d",&n1);
+scanf("\n%d",&n2);
+for(i=n1;i<=n2;i++)
+{
+temp=i;
+while(i!=0)
+{
+  a=i%10;
+  x=a*a*a;
+  sum=sum+x;
+  i=i/10;
+}
+if(sum==temp)
+{
+   printf("%d",temp);
+}
+}
+
+return 0;
+}
