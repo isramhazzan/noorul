fabba868a5124afd0167eede447dc23f7e16daf0
isramhazzan <35652605+isramhazzan@users.noreply.github.com>
2018-03-02 14:46:29 +0530

    Create 28 each elem.c

---
+++ 28 each elem.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[20],i;
+printf("Enter the value\n);
+for(i=0;i<20;i++)
+ {
+  scanf("%d",&a[i]);
+ }
+for(i=0;i<20;i++)
+ {
+  printf("%d",a[i]);
+  }
+ } 
