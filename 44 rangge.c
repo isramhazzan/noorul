abcd55b6d3fdd30471edc0ea618ab27f526ba278
isramhazzan <35652605+isramhazzan@users.noreply.github.com>
2018-03-20 16:15:01 +0530

    Create 44 rangge.c

---
+++ 44 rangge.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+char a[10];
+clrscr();
+printf("Enter the value \n",i);
+scanf("%d",&i);
+printf("enter the string:\n",a);
+scanf("%c",&a);
+if((i>=1)&&(i<=10))
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
